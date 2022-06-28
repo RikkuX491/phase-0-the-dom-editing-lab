125b8932d5b03b8be93854208f32c3fec5b9e35e
Ricardo Guerra <RikkuG491@gmail.com>
2022-06-28 10:49:43 -0400

    commit tree 2b5fad3f36558b8f364738d04222df90c6615da2

---
+++ index.js
@@ -0,0 +1,13 @@
+/*const heading = document.querySelector("h1");
+
+console.log(heading);
+heading.innerText = "Flatiron School is the best school!";
+
+const newElement = document.createElement("div");
+console.log(newElement);
+newElement.innerHTML = "<h2>I'm having so much fun now</h2>"
+console.log(newElement);
+
+const paragraph = document.getElementById("paragraph");
+console.log(paragraph);*/
+//paragraph.remove();
